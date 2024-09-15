be799dbe3124c694ad50902a5eb6073250b66423
qwertyuisdfghjkxcvbn <x@adiasta.com>
2024-09-15 02:43:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,27 @@
-const CACHE_NAME = 'word-finder-cache-v1';
+const CACHE_NAME = 'adiacta-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/style.css',
+    '/styles.css',
     '/script.js',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then((response) => {
+                return response || fetch(event.request);
+            })
     );
 });
